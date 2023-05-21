99389de1211cd425d7d09345cc08a203ce509c89
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-05-21 11:57:38 +0300

    updated task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-	char a[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, a, sizeof(a) - 1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
