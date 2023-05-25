4867185f09a132faf22c3a697eb324fd24489a0d
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-05-25 22:05:49 +0300

    second updated task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,15 +11,13 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 
-	/**
-	 * i = 0;
-	 *
+	i = 0;
+	/*
 	 * while (i < 10)
-	 * {
-	 * putchar(i);
-	 * }
-	 */
-
+	{
+		putchar(i);
+	}
+	*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
