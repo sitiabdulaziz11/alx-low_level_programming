121220bb52cd20ee4d764560914601d4e361beda
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-05-29 02:51:02 +0300

    updated task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,13 +11,15 @@ int main(void)
 	for (a = 1; a <= 100; a++)
 	{
 		if (a % 15 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (a % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else if (a % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else
-			printf("%d ", a);
+			printf("%d", a);
+		if (a != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
