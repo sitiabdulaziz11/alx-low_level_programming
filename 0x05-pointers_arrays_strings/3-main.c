d940fb083c8bd19233e16baedd9db41f681ea113
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-05-29 23:42:12 +0300

    remove file

--- 0x05-pointers_arrays_strings/3-main.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-int main(void)
-{
-	    char *str;
-
-	        str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-		    _puts(str);
-		        return (0);
-}
