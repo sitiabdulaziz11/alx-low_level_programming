c3f32c6bb83534176402363b1ea15b29e7aad6a0
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-06-05 19:21:39 +0300

    task 0 to 3

---
+++ 0x07-pointers_arrays_string/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - search for a character.
+ * @s: pointer or searched value
+ * @c: search for
+ * Return: s or null
+ */
+char *_strchr(char *s, char c)
+{
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return ('\0');
+}
