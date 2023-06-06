0a269606e071f551a2201e361bcca0f71287a841
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-06-06 18:16:27 +0300

    second updated

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,11 +7,11 @@ void print_chessboard(char (*a)[8])
 {
 	int d, g;
 
-	for (d = 0; d < 8; b++)
+	for (d = 0; d < 8; d++)
 	{
 		for (g = 0; g < 8; g++)
 		{
-			_putchar(a[d] * a[g]);
+			_putchar(a[d][g]);
 		}
 		_putchar('\n');
 	}
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_diagsums - prints the sum of the two diagonals of a square matrix.
  * @a: pointer
@@ -12,8 +13,8 @@ void print_diagsums(int *a, int size)
 	{
 		res1 = 0;
 		res2 = 0;
-		res1 = res1 + a[(size + 1) * b];
-		res2 = res2 + a[(size - 1) * (b + 1)];
+		res1 = res1 + a[(size + 1) * y];
+		res2 = res2 + a[(size - 1) * (y + 1)];
 	}
 	printf("%d, %d\n", res1, res2);
 }
