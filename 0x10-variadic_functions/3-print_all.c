8d522d927321ca8614e795f9f8043890bcda2c3b
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-06-23 10:59:19 +0300

    variable argument

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_all - print anything.
+ * @format: list of type of argument.
+ *
+ */
+void print_all(const char * const format, ...)
+{
+	va_list form;
+	int i, r = 0;
+	char c;
+	float f;
+	char *s;
+
+	va_start(form, format);
+	while (format[r] != '\0')
+	{
+		if (format[r] == 'c')
+		{
+			c = (char) va_arg(form, int);
+			printf("%c, ", c);
+		}
+		else if (format[r] == 'i')
+		{
+			i = va_arg(form, int);
+			printf("%d, ", i);
+		}
+		else if (format[r] == 'f')
+		{
+			f = va_arg(form, double);
+			printf("%f, ", f);
+		}
+		else if (format[r] == 's')
+		{
+			s = va_arg(form, char *);
+			if (s == NULL)
+				printf("(nil)");
+			else
+				printf("%s", s);
+		}
+		r++;
+	}
+	va_end(form);
+	printf("\n");
+}
