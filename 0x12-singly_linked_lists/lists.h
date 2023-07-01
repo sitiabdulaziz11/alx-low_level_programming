825427729e27199f1e0f09862f4d6199790f55ae
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-07-02 01:09:05 +0300

    header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -24,5 +25,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+int w_strlen(const char *p);
 
 #endif
