cc06afa4c274bab17c97b07ad0ee1f410b96a61a
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-07-07 09:28:46 +0300

    updated task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,7 +10,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int h;
 
-	if (n == NULL)
+	if (n == NULL || index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
 	h = 1UL << index;
