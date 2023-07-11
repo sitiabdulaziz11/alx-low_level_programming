9becdb17a86a9602e420bf65eeffe24c27152ae3
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-07-11 10:47:47 +0300

    second updated task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,9 +13,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-	fd = open("filename", O_RDONLY);
+
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
+
 	arr = malloc(sizeof(char) * letters);
 	if (arr == NULL)
 	{
