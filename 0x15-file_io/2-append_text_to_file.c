6411a9054ecec5fea13040163112a0a639441596
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-07-12 11:14:41 +0300

    second updated

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,11 +18,8 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	i = 0;
 	while (text_content[i] != '\0')
-	{
 		i++;
-	}
-	if (write(fd, text_content, i) == -1);
-	{
+	if (write(fd, text_content, i) == -1)
 		close(fd);
 		return (-1);
 	}
