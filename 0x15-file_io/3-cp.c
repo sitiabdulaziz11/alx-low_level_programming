5a9f1a2357c1fef0ebfaa7b18c338f69ee89fc33
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-07-13 10:05:22 +0300

    copy from one file to other file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,7 +12,7 @@ int main(int ac, char **argv)
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: %s cp file_from file_to\n", argv[0]);
-		exit(97);
+			exit(97);
 	}
 	copy_file(argv[1], argv[2]);
 
@@ -36,18 +36,21 @@ void copy_file(const char *f_from, const char *f_to)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", f_from);
 		exit(98);
 	}
-	fd_to = open(f_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
+	fd_to = open(f_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	while ((rdbyt = read(fd_from, arr, BU_SIZE)) > 0)
 	{
 		if (write(fd_to, arr, rdbyt) != rdbyt || fd_to == -1)
 		{
+			close(fd_from);
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", f_to);
 			exit(99);
 		}
 	}
 	if (rdbyt == -1)
 	{
+		close(fd_from);
+		close(fd_to);
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", f_from);
 		exit(98);
 	}
