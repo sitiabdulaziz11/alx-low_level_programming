28a0b22fe738d332a91a0401e8d865b433d3fc63
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-07-13 08:57:16 +0300

    file copy in file i/os

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,7 +11,7 @@ int main(int ac, char **argv)
 {
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: %s cp file_from file_to\n", argv[0]);
+		dprintf(STDERR_FILENO, "Usage: %s cp file_from file_to\n", argv[0]);
 		exit(97);
 	}
 	copy_file(argv[1], argv[2]);
@@ -33,14 +33,14 @@ void copy_file(const char *f_from, const char *f_to)
 	fd_from = open(f_from, O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", f_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", f_from);
 		exit(98);
 	}
 	fd_to = open(f_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		close(fd_from);
-		dprintf(2, "Error: Can't read from file %s\n", f_to);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", f_to);
 		exit(99);
 	}
 	while ((rdbyt = read(fd_from, arr, BU_SIZE)) > 0)
@@ -50,7 +50,7 @@ void copy_file(const char *f_from, const char *f_to)
 		{
 			close(fd_from);
 			close(fd_to);
-			dprintf(2, "Error: Can't write to %s\n", f_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", f_to);
 			exit(99);
 		}
 	}
@@ -58,12 +58,12 @@ void copy_file(const char *f_from, const char *f_to)
 	{
 		close(fd_from);
 		close(fd_to);
-		dprintf(2, "Error: Can't write to %s\n", f_from);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", f_from);
 		exit(98);
 	}
 	if (close(fd_from) == -1 || close(fd_to) == -1)
 	{
-		dprintf(2, "Error:Can't close fd %d\n", (fd_from == -1) ? fd_to : fd_from);
+		dprintf(STDERR_FILENO, "Error:Can't close fd %d\n", (fd_from == -1) ? fd_to : fd_from);
 		exit(100);
 	}
 }
