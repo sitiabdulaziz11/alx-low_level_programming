3933433c57a54f02473138f46a00ecc96149c41d
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-07-13 09:43:23 +0300

    copy from one file to other

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -27,8 +27,8 @@ int main(int ac, char **argv)
 void copy_file(const char *f_from, const char *f_to)
 {
 	int fd_from, fd_to;
-	int rdbyt, wtbyt;
-	char arr[1024];
+	int rdbyt;
+	char arr[BU_SIZE];
 
 	fd_from = open(f_from, O_RDONLY);
 	if (!f_from || fd_from == -1)
@@ -37,16 +37,10 @@ void copy_file(const char *f_from, const char *f_to)
 		exit(98);
 	}
 	fd_to = open(f_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
+
+	while ((rdbyt = read(fd_from, arr, BU_SIZE)) > 0)
 	{
-		close(fd_from);
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", f_to);
-		exit(99);
-	}
-	while ((rdbyt = read(fd_from, arr, 1024)) > 0)
-	{
-		wtbyt = write(fd_to, arr, rdbyt);
-		if (wtbyt == -1 || wtbyt != rdbyt)
+		if (write(fd_to, arr, rdbyt) != rdbyt || fd_to == -1)
 		{
 			close(fd_from);
 			close(fd_to);
@@ -61,9 +55,14 @@ void copy_file(const char *f_from, const char *f_to)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", f_from);
 		exit(98);
 	}
-	if (close(fd_from) == -1 || close(fd_to) == -1)
+	if (close(fd_from) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error:Can't close fd %d\n", (fd_from == -1) ? fd_from : fd_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
 }
