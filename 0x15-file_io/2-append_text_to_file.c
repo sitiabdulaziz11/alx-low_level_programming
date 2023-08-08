0a01fa1de20888a1dd205638985d423f149bab81
sitiabdulaziz <sitiabdulaziz462@gmail.com>
2023-08-08 10:28:26 +0300

    updayed task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,8 +10,10 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, i;
 
-	if (!filename || !text_content)
+	if (!filename && text_content == NULL)
 		return (-1);
+	if (filename && text_content == NULL)
+		return (1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
