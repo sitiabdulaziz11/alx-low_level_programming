e39952432129267b1f36ac0a1b5171362bab9cf2
sitiabdulaziz <sitiabdulaziz@gmail.com>
2023-08-18 18:43:10 +0300

    insert node @idx

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,13 +10,40 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *ptr;
+	dlistint_t *ptr, *tmp;
 	unsigned int i;
 
 	if (*h == NULL)
 		return (NULL);
 
-	if (idx == 0)
+	ptr = malloc(sizeof(dlistint_t));
+	if (ptr == NULL)
+		return (NULL);
+	ptr->n = n;
+	ptr->prev = NULL;
+	ptr->next = NULL;
 
-	
-		if (idx == )
+	if (idx == 0)
+	{
+		(*h)->prev = ptr;
+		ptr->next = *h;
+		*h = ptr;
+		return (ptr);
+	}
+	tmp = *h;
+	i = 0;
+	while (tmp != NULL)
+	{
+		if (i == idx)
+		{
+			tmp->prev = ptr;
+			ptr->next = tmp;
+			tmp = ptr;
+			return (ptr);
+		}
+		tmp = tmp->next;
+		i++;
+	}
+	return (ptr);
+}
+
