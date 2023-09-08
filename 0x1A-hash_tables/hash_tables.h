cee39a2c1bb1a651422fae94fdfd29c231746243
sitiabdulaziz <sitiabdulaziz462@gmail.com>
2023-09-08 20:57:32 +0300

    header file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -15,8 +15,8 @@
 typedef struct hash_node_s
 {
 	     char *key;
-	          char *value;
-		       struct hash_node_s *next;
+	     char *value;
+	     struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -30,7 +30,7 @@ typedef struct hash_node_s
 typedef struct hash_table_s
 {
 	     unsigned long int size;
-	          hash_node_t **array;
+	     hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
