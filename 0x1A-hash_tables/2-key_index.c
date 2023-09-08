62ebdf58376e2c9edfca53fc61b6e395e61075e6
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-09-08 06:39:22 +0000

    index of key

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int p;
+
+	hash = 5381;
+
+	while ((p = *str++))
+		hash = ((hash << 5) + hash) + p;
+
+	return (hash);
+}
+/**
+ * key_index -  function that gives you the index of a key.
+ * @key: key
+ * @size: size of the array.
+ * Returns the index at which the key/value should be stord.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int h_value;
+	unsigned long int index;
+
+	h_value = hash_djb2(key);
+	index = h_value % size;
+
+	return (index);
+}
