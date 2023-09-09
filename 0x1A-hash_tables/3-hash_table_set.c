3c1fb58bde2f39bf1ccfaeb722dedd8fbd831d20
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-09-09 09:25:42 +0000

    updated set hash

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,31 +12,63 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int indx = 0;
 	hash_node_t *newn = NULL, *dt = NULL;
+	char *newv;
 
-	if (ht == NULL || key == NULL)
+	if (ht == NULL || key == NULL || ht->array == NULL || ht->size == 0 ||
+			strlen(key) == 0 || value == NULL)
 		return (0);
 
-	indx = key_index((unsigned char *)key, ht->size);
+	indx = key_index((const unsigned char *)key, ht->size);
 	dt = ht->array[indx];
 
 	while (dt != NULL)
 	{
 		if (strcmp(key, dt->key) == 0)
 		{
+			newv = strdup(value);
+			if (newv == NULL)
+				return (0);
 			free(dt->value);
-			dt->value = strdup(value);
+			dt->value = newv;
 			return (1);
 		}
+		dt = dt->next;
 	}
 
-	newn = malloc(sizeof(hash_node_t));
-
+	newn = create_hash_node(key, value);
 	if (newn == NULL)
 		return (0);
 
-	newn->key = strdup(key);
-	newn->value = strdup(value);
-	newn->next = dt;
-	dt = newn;
+	newn->next = ht->array[indx];
+	ht->array[indx] = newn;
 	return (1);
+}
+/**
+ * create_hash_node - create new hash node.
+ * @value: value.
+ * @key: key
+ * Return: new node
+ */
+hash_node_t *create_hash_node(const char *key, const char *value)
+{
+	hash_node_t *nd;
+
+	nd = malloc(sizeof(hash_node_t));
+	if (nd == NULL)
+		return (NULL);
+	nd->key = strdup(key);
+	if (nd->key == NULL)
+	{
+		free(nd);
+		return (NULL);
+	}
+	nd->value = strdup(value);
+	if (nd->value == NULL)
+	{
+		free(nd->key);
+		free(nd);
+		return (NULL);
+	}
+	nd->next = NULL;
+	return (nd);
 }
