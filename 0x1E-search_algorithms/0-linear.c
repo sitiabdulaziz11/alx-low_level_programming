5369892591bff2f5b213338ad2629d2f97d0065b
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-01-29 16:22:26 +0300

    c back again with linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -16,11 +16,11 @@ int linear_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
-	
+
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		
+
 		if (array[i] == value)
 			return (i);
 	}
